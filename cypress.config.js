78f4f03690d479cd0ed084c4242d2fb6610b49ca
Shawkat Hossain <36979745+sohanpro@users.noreply.github.com>
2024-01-03 12:33:11 +0600

    fix cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   
   e2e: {
-    baseUrl: 'https://hrm.aamarpay.dev/login-form',
+    baseUrl: 'https://hrm.aamarpay.dev',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
