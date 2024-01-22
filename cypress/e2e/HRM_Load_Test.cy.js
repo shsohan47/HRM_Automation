b05f8b975069548ce5f110dae81a61480f63e8a5
Shawkat Hossain <36979745+sohanpro@users.noreply.github.com>
2024-01-22 13:35:23 +0600

    :construction_worker: Construct Registration Panel

--- cypress/e2e/HRM_Load_Test.cy.js
+++ cypress/e2e/HRM_Load_Test.cy.js
@@ -1,22 +1,22 @@
 import "cypress-file-upload";
 
+describe("Load Test", () => {
+  let CompanyNameCount = 1;
+  let CompanyDetailsCount = 1;
+  let CompanyHRCount = 1;
+  let CompanyHR_emailCount = 1;
 
-describe('Load Test', () => {
-  let count  = 1;
   //Company name counter
-  function generaterandomCompanyname()
-  {
-    var name = "company" + count;
-    count++;
+  function generaterandomCompanyname() {
+    var name = "company" + CompanyNameCount;
+    CompanyNameCount++;
     return name;
   }
 
-
   //Company Details Counter
-  function generaterandomCompanyDetails()
-  {
-    var Details = "companyDetails" + count;
-    count++;
+  function generaterandomCompanyDetails() {
+    var Details = "companyDetails" + CompanyDetailsCount;
+    CompanyDetailsCount++;
     return Details;
   }
 
@@ -31,20 +31,64 @@ describe('Load Test', () => {
     });
   }
 
-  it('passes', () => {
-    cy.visit('/registration-form');
+  //generate mobile number
+  function GenerateMobileNumber() {
+    const firstThreeDigits = "019";
+    const randomPart = Math.floor(Math.random() * Math.pow(10, 8))
+      .toString()
+      .padStart(8, "0");
+    return firstThreeDigits + randomPart;
+  }
 
-    for (let i=0;i<1000;i++)
-    {
-      cy.visit('/registration-form');
-      cy.get('[name="companyName"]').type(generaterandomCompanyname())
-      cy.get('[name="companyDetails"]').type(generaterandomCompanyDetails());
-      cy.get('[name="address"]').type("Demo Address");
-      FileUpload("Picture/test.jpeg","test.jpeg");
+  //Generate HR unique name
+  function generaterandomHRname() {
+    var name = "HR" + CompanyHRCount;
+
+    CompanyHRCount++;
+    return name;
+  }
+
+  //Generate HR unique email
+  function generaterandomHRemail() {
+    var name = "HR" + CompanyHR_emailCount + "@gmail.com";
+    CompanyHR_emailCount++;
+    return name;
+  }
+
+  it("passes", () => {
+    cy.visit("/registration-form");
+
+    for (let i = 0; i < 1000; i++) {
+      cy.visit("/registration-form");
+      cy.get('[name="companyName"]').type(generaterandomCompanyname(),{force:true});
+      cy.get('[name="companyDetails"]').type(generaterandomCompanyDetails(),{force:true});
+      cy.get('[name="address"]').type("Demo Address",{force:true});
+      FileUpload("Picture/test.jpeg", "test.jpeg",{force:true});
+      cy.get('[name="contactNumber"]').type(GenerateMobileNumber(),{force:true});
+      cy.get(".next").click({force:true});
+      cy.wait(1000);
+      cy.get(
+        '[style="display: block; left: 0%; opacity: 1;"] > [type="text"]'
+      ).type(generaterandomHRname(),{force:true});
+      cy.get('[type="email"]').type(generaterandomHRemail(),{force:true});
+      cy.get("#password").type("123456789",{force:true});
+      cy.get("#password_confirmation").type("123456789",{force:true});
+
+      cy.get('[type="email"]').invoke('val').as("emailId");
+      cy.get("#password").invoke('val').as("passwordId");
+        cy.get('@emailId').then((id)=>
+        {
+          cy.log("email: ", id);
+        })
+      cy.get('@passwordId').then((pass)=>{
+        cy.log("password: ", pass);
+      })
       
-      
 
+      //Submit button
 
+      //Comment the button for user permission
+      //cy.get("#submitBtn").click({force:true});
     }
-  })
-})
+  });
+});
